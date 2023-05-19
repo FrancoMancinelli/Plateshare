f41e1cdd791f076c1f2c842581db8fe0d9c5dfda
FrancoMancinelli <franco.e.mancinelli@gmail.com>
2023-05-19 20:28:17 +0200

    Nuevos colores añadidos

--- lib/util/AppColors.dart
+++ lib/util/AppColors.dart
@@ -7,6 +7,7 @@ class AppColors {
   static const Color orangeColor = Color(0xFFFD9A00);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
+  static const Color greyColor = Color(0xFF7D776F);
   static const Color brownTextColor = Color(0xFF4F3119);
   static const Color brownRecepieColor = Color(0xFFCBB79F);
   static const Color recommendationColor = Color(0xFFCCDDD7);
