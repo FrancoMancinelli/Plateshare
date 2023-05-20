cdf27a08f75093b3dbd7b857b3f99fdf12967c26
FrancoMancinelli <franco.e.mancinelli@gmail.com>
2023-05-21 01:50:27 +0200

    Creado widget de comentarios

--- lib/screens/RecipeDetails.dart
+++ lib/screens/RecipeDetails.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:plateshare/util/AppColors.dart';
+import 'package:plateshare/widgets/IngredientContainer.dart';
+import 'package:plateshare/widgets/InstructionsContainer.dart';
 
 import 'InicioScreen.dart';
 
@@ -17,8 +19,38 @@ class RecipeDetailsScreen extends StatefulWidget {
 }
 
 var screenSize;
+int flag = 1;
 
 class _RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
+  Color firstButtonColor = AppColors.lightBrownRecipe;
+  Color secondButtonColor = AppColors.brownButtonsRecipe;
+
+  void handleFirstButtonPressed() {
+    setState(() {
+      secondButtonColor = AppColors.brownButtonsRecipe;
+      firstButtonColor = AppColors.brownInfoRecipe;
+      flag = 1;
+    });
+  }
+
+  void handleSecondButtonPressed() {
+    setState(() {
+      firstButtonColor = AppColors.brownButtonsRecipe;
+      secondButtonColor = AppColors.brownInfoRecipe;
+      flag = 2;
+    });
+  }
+
+  Widget buildContainerBasedOnFlag(int flag) {
+    if (flag == 1) {
+      return const IngredientContainer();
+    } else if (flag == 2) {
+      return const InstructionsContainer();
+    } else {
+      return Container();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     screenSize = MediaQuery.of(context).size;
@@ -136,156 +168,225 @@ class _RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
                 Container(
                   height: 1000,
                   color: AppColors.accentColor,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 5, 0, 0),
-                          child: Text(
-                            'Titulo de la receta en cuestion',
-                            style: GoogleFonts.acme(
-                              textStyle: const TextStyle(
-                                fontSize: 32,
-                                color: AppColors.brownTextColor,
-                                fontFamily: 'Acme',
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 5, 0, 0),
+                            child: Text(
+                              'Titulo de la receta en cuestion',
+                              style: GoogleFonts.acme(
+                                textStyle: const TextStyle(
+                                  fontSize: 34,
+                                  color: AppColors.brownTextColor,
+                                  fontFamily: 'Acme',
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(8, 3, 10, 0),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.star_rounded,
-                              color: AppColors.brownInfoRecipe,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(1, 2, 0, 0),
-                              child: Text(
-                                '4.5',
-                                style: GoogleFonts.acme(
-                                  textStyle: const TextStyle(
-                                    fontSize: 16,
-                                    color: AppColors.brownInfoRecipe,
-                                    fontFamily: 'Acme',
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 3, 10, 0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.star_rounded,
+                                color: AppColors.brownInfoRecipe,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(1, 2, 0, 0),
+                                child: Text(
+                                  '4.5',
+                                  style: GoogleFonts.acme(
+                                    textStyle: const TextStyle(
+                                      fontSize: 16,
+                                      color: AppColors.brownInfoRecipe,
+                                      fontFamily: 'Acme',
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Icon(
-                              Icons.access_time,
-                              color: AppColors.brownInfoRecipe,
-                              size: 20,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(3, 2, 0, 0),
-                              child: Text(
-                                '60 mins',
-                                style: GoogleFonts.acme(
-                                  textStyle: const TextStyle(
-                                    fontSize: 16,
-                                    color: AppColors.brownInfoRecipe,
-                                    fontFamily: 'Acme',
-                                  ),
-                                ),
+                              SizedBox(
+                                width: 10,
                               ),
-                            ),
-                            Spacer(), // Add a spacer to push the next elements to the right
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 4, 2, 0),
-                              child: Icon(
-                                Icons.chat_bubble_outline,
+                              Icon(
+                                Icons.access_time,
                                 color: AppColors.brownInfoRecipe,
                                 size: 20,
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 2, 0, 0),
-                              child: Text(
-                                '0 Comentarios',
-                                style: GoogleFonts.acme(
-                                  textStyle: const TextStyle(
-                                    fontSize: 16,
-                                    color: AppColors.brownInfoRecipe,
-                                    fontFamily: 'Acme',
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(3, 2, 0, 0),
+                                child: Text(
+                                  '60 mins',
+                                  style: GoogleFonts.acme(
+                                    textStyle: const TextStyle(
+                                      fontSize: 16,
+                                      color: AppColors.brownInfoRecipe,
+                                      fontFamily: 'Acme',
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(50, 10, 50, 0),
-                        child: Container(
-                          height: 48,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20.0),
-                            color: AppColors.brownButtonsRecipe,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              TextButton(
-                                style: ButtonStyle(
-                                  shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20.0),
-                                    ),
-                                  ),
-                                  backgroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          AppColors.lightBrownRecipe),
-
+                              Spacer(), // Add a spacer to push the next elements to the right
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 4, 2, 0),
+                                child: Icon(
+                                  Icons.chat_bubble_outline,
+                                  color: AppColors.brownInfoRecipe,
+                                  size: 20,
                                 ),
-                                onPressed: () {
-                                  // Handle button 1 press
-                                },
-                                child: Text('Ingredientes (0)', style: GoogleFonts.acme(
-                                  textStyle: const TextStyle(
-                                    fontSize: 18,
-                                    color: AppColors.whiteColor,
-                                    fontFamily: 'Acme',
-                                  ),
-                                ),),
                               ),
-                              TextButton(
-                                style: ButtonStyle(
-                                  shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(50.0),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 2, 0, 0),
+                                child: Text(
+                                  '0 Comentarios',
+                                  style: GoogleFonts.acme(
+                                    textStyle: const TextStyle(
+                                      fontSize: 16,
+                                      color: AppColors.brownInfoRecipe,
+                                      fontFamily: 'Acme',
                                     ),
                                   ),
-                                  backgroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          AppColors.brownButtonsRecipe),
-
                                 ),
-                                onPressed: () {
-                                  // Handle button 2 press
-                                },
-                                child: Text('Instrucciones', style: GoogleFonts.acme(
-                                  textStyle: const TextStyle(
-                                    fontSize: 18,
-                                    color: AppColors.whiteColor,
-                                    fontFamily: 'Acme',
-                                  ),
-                                ),),
                               ),
                             ],
                           ),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(30, 10, 30, 0),
+                          child: Container(
+                            height: 48,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20.0),
+                              color: AppColors.brownButtonsRecipe,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                TextButton(
+                                  style: ButtonStyle(
+                                    shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(20.0),
+                                      ),
+                                    ),
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            firstButtonColor),
+                                  ),
+                                  onPressed: handleFirstButtonPressed,
+                                  child: Text(
+                                    'Ingredientes (0)',
+                                    style: GoogleFonts.acme(
+                                      textStyle: const TextStyle(
+                                        fontSize: 18,
+                                        color: AppColors.whiteColor,
+                                        fontFamily: 'Acme',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                TextButton(
+                                  style: ButtonStyle(
+                                    shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(50.0),
+                                      ),
+                                    ),
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            secondButtonColor),
+                                  ),
+                                  onPressed: handleSecondButtonPressed,
+                                  child: Text(
+                                    'Instrucciones',
+                                    style: GoogleFonts.acme(
+                                      textStyle: const TextStyle(
+                                        fontSize: 18,
+                                        color: AppColors.whiteColor,
+                                        fontFamily: 'Acme',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        buildContainerBasedOnFlag(flag),
+
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 30, 20, 10),
+                          child: Container(
+                            color: AppColors.greyColor,
+                            height: 2,
+                            width: screenSize.width,
+                          ),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 0, 12, 0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.star_rounded,
+                                color: AppColors.brownInfoRecipe,
+                                size: 36,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(1, 2, 0, 0),
+                                child: Text(
+                                  '4.5',
+                                  style: GoogleFonts.acme(
+                                    textStyle: const TextStyle(
+                                      fontSize: 30,
+                                      color: AppColors.brownInfoRecipe,
+                                      fontFamily: 'Acme',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Spacer(),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 2, 0, 0),
+                                child: Text(
+                                  '57 Valoraciones',
+                                  style: GoogleFonts.acme(
+                                    textStyle: const TextStyle(
+                                      fontSize: 30,
+                                      color: AppColors.brownInfoRecipe,
+                                      fontFamily: 'Acme',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(15, 5, 15, 0),
+                          child: Container(
+                            height: 80,
+                            decoration: BoxDecoration(
+                              color: AppColors.brownButtonsRecipe,
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                          ),
+                        ),
+
+                        //END
+                      ],
+                    ),
                   ),
                 ),
               ],
---
+++ lib/widgets/RecipeComment.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:like_button/like_button.dart';
+import 'package:plateshare/screens/LoginScreen.dart';
+import 'package:plateshare/screens/RecipeDetails.dart';
+import 'package:plateshare/services/firebase_service.dart';
+import 'package:plateshare/util/AppColors.dart';
+
+class RecipeComment extends StatefulWidget {
+  const RecipeComment({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _RecipeCommentState createState() => _RecipeCommentState();
+}
+
+class _RecipeCommentState extends State<RecipeComment> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5, 5, 5, 10),
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.brownButtonsRecipe,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        height: 130,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2.0,
+                      ),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image.network(
+                      'https://firebasestorage.googleapis.com/v0/b/plateshare-tfg2023.appspot.com/o/default_profile_pic2.png?alt=media&token=61ad1f5c-b211-4068-b3da-feccba2f4b3e',
+                      width: 40,
+                      height: 40,
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Administrador',
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: 'Acme',
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '@admin',
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontFamily: 'Acme',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                    //MÁXIMO 130 CARACTERES
+                    child: Text(
+                      'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontFamily: 'Acme',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
