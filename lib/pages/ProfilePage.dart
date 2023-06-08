dc511ad969023e3312bf3ae4524685e9281ab4bb
FrancoMancinelli <franco.e.mancinelli@gmail.com>
2023-06-08 22:01:23 +0200

    Mejora de interfaz en el Perfil

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -95,216 +95,252 @@ class _ProfilePageState extends State<ProfilePage> {
 
     return SafeArea(
       child: Container(
-        width: screenSize.width,
-        height: screenSize.height,
-        decoration: const BoxDecoration(
-          color: AppColors.accentColor,
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: 50,
-                    width: screenSize.width,
-                    color: AppColors.primaryColor,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 5),
-                          child: IconButton(
-                            icon: Icon(
-                              Icons.arrow_back_ios,
-                              color: AppColors.primaryColor,
-                              size: 20,
-                            ),
-                            onPressed: () {
-                              // Handle left icon button press
-                            },
-                          ),
-                        ),
-                        Text(
-                          '@${widget.usernameData}',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(right: 5),
-                          child: IconButton(
-                            icon: Icon(
-                              Icons.add_rounded,
-                              color: Colors.white,
-                              size: 30,
-                            ),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => RecipeFormScreenOne(
-                                      emailData: widget.emailData,
-                                      nameData: widget.nameData,
-                                      profilePicData: widget.profilePicData,
-                                      usernameData: widget.usernameData),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
+        color: AppColors.accentColor,
+        child: CustomScrollView(
+          slivers: [
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: _ProfileHeaderDelegate(
+                username: widget.usernameData,
+                onBackButtonPressed: () {
+                  // Handle left icon button press
+                },
+                onAddButtonPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RecipeFormScreenOne(
+                        emailData: widget.emailData,
+                        nameData: widget.nameData,
+                        profilePicData: widget.profilePicData,
+                        usernameData: widget.usernameData,
+                      ),
                     ),
-                  ),
-                ],
+                  );
+                },
               ),
-              SizedBox(height: 20), // Add spacing between the row and the image
-              Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 2,
-                  ),
-                ),
-                child: CircleAvatar(
-                  radius: 45,
-                  backgroundImage: NetworkImage(widget.profilePicData),
-                ),
-              ),
-
-              SizedBox(height: 10),
-              Text(
-                '${widget.nameData}',
-                style: GoogleFonts.acme(
-                  textStyle: const TextStyle(
-                    fontSize: 24,
-                    color: AppColors.blackColor,
-                    fontFamily: 'Acme',
-                  ),
-                ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+            ),
+            SliverToBoxAdapter(
+              child: SingleChildScrollView(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: data.map((item) {
-                        return Column(
-                          children: [
-                            Text(
-                              item['count'],
-                              style: GoogleFonts.acme(
-                                textStyle: const TextStyle(
-                                  fontSize: 16,
-                                  color: AppColors.blackColor,
-                                  fontFamily: 'Acme',
-                                ),
-                              ),
-                            ),
-                            Text(
-                              item['label'],
-                              style: GoogleFonts.acme(
-                                textStyle: const TextStyle(
-                                  fontSize: 18,
-                                  color: AppColors.greyColor,
-                                  fontFamily: 'Acme',
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      }).toList(),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 15, 25, 0),
-                child: Container(
-                  color: AppColors.greyColor,
-                  width: screenSize.width,
-                  height: 1,
-                ),
-              ),
-
-              Align(
-                alignment: Alignment.center,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextButton(
-                        onPressed: handleMenuButtonTap,
-                        style: TextButton.styleFrom(
-                          backgroundColor: menuButtonColor,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25.0),
-                          ),
-                          minimumSize: Size(150, 20),
-                        ),
-                        child: Icon(Icons.restaurant_menu_rounded,
-                            size: 24, color: AppColors.blackColor),
-                      ),
-                    ),
+                    SizedBox(
+                        height: 20), // Add spacing between the row and the image
                     Container(
-                      height: 25,
-                      width: 1,
-                      color: AppColors.greyColor,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextButton(
-                        onPressed: handleFavoriteButtonTap,
-                        style: TextButton.styleFrom(
-                          backgroundColor: favoriteButtonColor,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25.0),
-                          ),
-                          minimumSize: Size(150, 20),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
                         ),
-                        child: Icon(Icons.favorite_border_outlined,
-                            size: 24, color: AppColors.blackColor),
+                      ),
+                      child: CircleAvatar(
+                        radius: 45,
+                        backgroundImage: NetworkImage(widget.profilePicData),
                       ),
                     ),
+                    SizedBox(height: 10),
+                    Text(
+                      '${widget.nameData}',
+                      style: GoogleFonts.acme(
+                        textStyle: const TextStyle(
+                          fontSize: 24,
+                          color: AppColors.blackColor,
+                          fontFamily: 'Acme',
+                        ),
+                      ),
+                    ),
+      
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: data.map((item) {
+                              return Column(
+                                children: [
+                                  Text(
+                                    item['count'],
+                                    style: GoogleFonts.acme(
+                                      textStyle: const TextStyle(
+                                        fontSize: 16,
+                                        color: AppColors.blackColor,
+                                        fontFamily: 'Acme',
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    item['label'],
+                                    style: GoogleFonts.acme(
+                                      textStyle: const TextStyle(
+                                        fontSize: 18,
+                                        color: AppColors.greyColor,
+                                        fontFamily: 'Acme',
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(25, 15, 25, 0),
+                      child: Container(
+                        color: AppColors.greyColor,
+                        width: screenSize.width,
+                        height: 1,
+                      ),
+                    ),
+      
+                    Align(
+                      alignment: Alignment.center,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextButton(
+                              onPressed: handleMenuButtonTap,
+                              style: TextButton.styleFrom(
+                                backgroundColor: menuButtonColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                minimumSize: Size(150, 20),
+                              ),
+                              child: Icon(Icons.restaurant_menu_rounded,
+                                  size: 24, color: AppColors.blackColor),
+                            ),
+                          ),
+                          Container(
+                            height: 25,
+                            width: 1,
+                            color: AppColors.greyColor,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextButton(
+                              onPressed: handleFavoriteButtonTap,
+                              style: TextButton.styleFrom(
+                                backgroundColor: favoriteButtonColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                                minimumSize: Size(150, 20),
+                              ),
+                              child: Icon(Icons.favorite_border_outlined,
+                                  size: 24, color: AppColors.blackColor),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+      
+                    Column(
+                      children: [
+                        if (flag == 1)
+                          MyRecipesContainer(
+                            emailData: widget.emailData,
+                            nameData: widget.nameData,
+                            profilePicData: widget.profilePicData,
+                            recipesIDs: widget.recipesIDs,
+                            userId: widget.userId,
+                            usernameData: widget.usernameData,
+                          ),
+                        if (flag == 2)
+                          MyFavoritesContainer(
+                            emailData: widget.emailData,
+                            nameData: widget.nameData,
+                            profilePicData: widget.profilePicData,
+                            likedRecipesIDs: widget.likedRecipesIDs,
+                            usernameData: widget.usernameData,
+                            userId: widget.userId,
+                          ),
+                      ],
+                    )
+      
+                    //
                   ],
                 ),
               ),
-
-              Column(
-                children: [
-                  if (flag == 1)
-                    MyRecipesContainer(
-                      emailData: widget.emailData,
-                      nameData: widget.nameData,
-                      profilePicData: widget.profilePicData,
-                      recipesIDs: widget.recipesIDs,
-                      userId: widget.userId,
-                      usernameData: widget.usernameData,
-                    ),
-                  if (flag == 2)
-                    MyFavoritesContainer(
-                      emailData: widget.emailData,
-                      nameData: widget.nameData,
-                      profilePicData: widget.profilePicData,
-                      likedRecipesIDs: widget.likedRecipesIDs,
-                      usernameData: widget.usernameData,
-                      userId: widget.userId,
-                    ),
-                ],
-              )
-
-              //
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class _ProfileHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final String username;
+  final VoidCallback onBackButtonPressed;
+  final VoidCallback onAddButtonPressed;
+
+  _ProfileHeaderDelegate({
+    required this.username,
+    required this.onBackButtonPressed,
+    required this.onAddButtonPressed,
+  });
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      height: 50,
+      color: AppColors.primaryColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 5),
+            child: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios,
+                color: AppColors.primaryColor,
+                size: 20,
+              ),
+              onPressed: onBackButtonPressed,
+            ),
+          ),
+          Text(
+            '@$username',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: IconButton(
+              icon: Icon(
+                Icons.add_rounded,
+                color: Colors.white,
+                size: 30,
+              ),
+              onPressed: onAddButtonPressed,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 50;
+
+  @override
+  double get minExtent => 50;
+
+  @override
+  bool shouldRebuild(_ProfileHeaderDelegate oldDelegate) {
+    return username != oldDelegate.username ||
+        onBackButtonPressed != oldDelegate.onBackButtonPressed ||
+        onAddButtonPressed != oldDelegate.onAddButtonPressed;
   }
 }
