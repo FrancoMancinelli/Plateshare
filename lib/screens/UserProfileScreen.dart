6035887823a414c63881407685194616041891ea
FrancoMancinelli <franco.e.mancinelli@gmail.com>
2023-06-09 20:52:10 +0200

    Creado el perfil de otros usuarios

---
+++ lib/screens/UserProfileScreen.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:lottie/lottie.dart';
+import 'package:plateshare/screens/InicioScreen.dart';
+import 'package:plateshare/services/firebase_service.dart';
+import 'package:http/http.dart' as http;
+
+import '../util/AppColors.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  final String emailData;
+  final String nameData;
+  final String usernameData;
+  final String profilePicData;
+
+  const UserProfileScreen({
+    Key? key,
+    required this.emailData,
+    required this.nameData,
+    required this.usernameData,
+    required this.profilePicData,
+  }) : super(key: key);
+
+  @override
+  _UserProfileScreenState createState() => _UserProfileScreenState();
+}
+
+var screenSizeContactScreen;
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  TextEditingController _asuntoController = TextEditingController();
+  TextEditingController _mensajeController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    screenSizeContactScreen = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              color: AppColors.primaryColor,
+              height: 50,
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios,
+                      color: AppColors.primaryColor,
+                    ),
+                    onPressed: () {},
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        '@${widget.usernameData}',
+                        style: GoogleFonts.acme(
+                          textStyle: const TextStyle(
+                            color: AppColors.whiteColor,
+                            fontSize: 24,
+                            fontFamily: 'Acme',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.close,
+                      color: AppColors.whiteColor,
+                      size: 22,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
